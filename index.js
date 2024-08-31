fd88af450915aa7d2320e145f51727e89f1576b6
Walkoud <38588921+Walkoud@users.noreply.github.com>
2024-08-31 15:58:40 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 const axios = require('axios');
 const fs = require('fs');
-const ytdl = require('@distube/ytdl-core');
-const Tiktok = require("@tobyg74/tiktok-api-dl");
-const instagramDl = require("@sasmeee/igdl");
-const { TwitterDL } = require("twitter-downloader");
+
+
+const {alldown} = require("nayan-media-downloader");
+
+
+
+// Liste des URL des plateformes de vidéos
+const videoPlatforms = [
+  "https://www.facebook.com",
+  "https://facebook.com",
+  "https://www.tiktok.com",
+  "https://tiktok.com",
+  "https://www.x.com",
+  "https://x.com",
+  "https://www.twitter.com",
+  "https://twitter.com",
+  "https://www.instagram.com",
+  "https://instagram.com",
+  "https://www.youtube.com",
+  "https://youtube.com",
+  "https://youtu.be",
+  "https://www.pinterest.com",
+  "https://pinterest.com",
+  "https://drive.google.com",
+  "https://www.google.com/drive",
+  "https://www.capcut.com",
+  "https://capcut.com",
+  "https://www.likee.video",
+  "https://likee.video",
+  "https://www.threads.net",
+  "https://threads.net"
+];
+
+// Fonction pour vérifier si un lien correspond à une vidéo
+const isVideoLink = (link) => {
+  return videoPlatforms.some(platform => link.startsWith(platform));
+};
+
+
 
 const defaultConfig = {
   autocrop: false, // Paramètre par défaut
@@ -15,59 +50,24 @@ const MediaDownloader = async (url, options = {}) => {
   if (!url || !url.includes("http")) {
     throw new Error("Please specify a video URL...");
   }
+
   url = extractUrlFromString(url);
+
+  if (!isVideoLink(url) ) { 
+
+    const videofile = await downloadDirectVideo(url, config);       // Try .mp4 url or something like that
+
+    if(videofile){
+      return videofile;
+    } else {
+      throw new Error("URL not supported. Please provide a video URL from a valid platform.");
+    }
+  }
+
   await deleteTempVideos();
 
-  if (url.includes("instagram.com/")) {
-    try {
-      const dataList = await instagramDl(url);
-      if (!dataList || !dataList[0]) {
-        throw new Error("Error: Invalid video URL...");
-      }
-      const videoURL = dataList[0].download_link;
-      const videofile = await downloadDirectVideo(videoURL, config);
-
-      return videofile;
-
-    } catch (error) {
-      throw new Error("Error downloading or sending Instagram video: " + error.message);
-    }
-
-  } else if (url.includes('tiktok.com/')) {
-    try {
-      const result = await Tiktok.Downloader(url, {
-        version: "v2" // version: "v1" | "v2" | "v3"
-      });
-
-      const videoLink = result.result.video;
-      const videofile = await downloadDirectVideo(videoLink, config);
-
-      return videofile;
-    } catch (error) {
-      throw new Error("Error downloading TikTok video: " + error.message);
-    }
-
-  } else if (url.includes("youtu.be/") || url.includes("youtube.com/")) {
-    try {
-      const videoLink = await downloadYoutubeVideo(url, config);
-      return videoLink;
-    } catch (error) {
-      throw new Error("Error downloading YouTube video: " + error.message);
-    }
-
-  } else if (url.includes("twitter.com") || url.includes("x.com/")) {
-    try {
-      const result = await TwitterDL(url);
-      const videoLink = result.result.media[0].videos[result.result.media[0].videos.length - 1].url;
-      const videofile = await downloadDirectVideo(videoLink, config);
-
-      return videofile;
-    } catch (error) {
-      throw new Error("Error downloading Twitter video: " + error.message);
-    }
-
-  } else if (url.includes("http")) {
-    const videofile = await downloadDirectVideo(url, config);
+ if (url.includes("http")) {
+    const videofile = await downloadSmartVideo(url, config);
 
     return videofile;
   } else {
@@ -75,6 +75,67 @@ const MediaDownloader = async (url, options = {}) => {
   }
 };
 
+async function downloadSmartVideo(url, config) {
+  try {
+
+
+    let data = await alldown(url)
+
+      if(!data || !data.data){
+        throw new Error("Can't download this link.");
+      }
+      if(data.data.low){
+        url = data.data.low
+      } else if(data.data.high){
+        url = data.data.high
+      } else {
+        throw new Error("Can't download this link.");
+      }
+    
+
+    const response = await axios({
+      url: url,
+      method: 'GET',
+      responseType: 'stream'
+    });
+
+    // Check if the downloaded content is a MP4 video
+    const contentType = response.headers['content-type'];
+
+    // Generate a unique file name
+    let fileName = 'temp_video.mp4';
+    let count = 1;
+    while (fs.existsSync(fileName)) {
+      fileName = `temp_video_${count}.mp4`;
+      count++;
+    }
+
+    // Create a write stream to save the video
+    const videoWriter = fs.createWriteStream(fileName);
+    response.data.pipe(videoWriter);
+
+    // Return a promise that resolves when the download is finished
+    return new Promise((resolve, reject) => {
+      videoWriter.on('finish', async () => {
+        if (config.autocrop) {
+          try {
+            const croppedFileName = await autoCrop(fileName);
+            resolve(croppedFileName);
+          } catch (error) {
+            reject(error);
+          }
+        } else {
+          resolve(fileName);
+        }
+      });
+      videoWriter.on('error', (error) => reject(error));
+    });
+
+  
+  } catch (error) {
+    throw new Error(`An error occurred while downloading video: ${error.message}`);
+  }
+}
 async function downloadDirectVideo(url, config) {
   try {
     const response = await axios({
@@ -119,82 +180,7 @@ async function downloadDirectVideo(url, config) {
   }
 }
 
-async function downloadYoutubeVideo(url, config) {
-  try {
-    const info = await ytdl.getInfo(url);
-    const formats = ytdl.filterFormats(info.formats, 'videoandaudio');
 
-    // Choose the format with the highest quality up to 25MB
-    let format;
-    for (let i = 0; i < formats.length; i++) {
-      const currentFormat = formats[i];
-      let contentLength = currentFormat?.contentLength;
-      if (!contentLength) {
-        contentLength = await getContentLength(currentFormat.url);
-        currentFormat.contentLength = contentLength;
-      }
-
-      if (contentLength && contentLength <= 25 * 1024 * 1024) {
-        format = currentFormat;
-        break;
-      }
-    }
-
-    if (!format) {
-      throw new Error('No suitable format found within 25 MB.');
-    }
-
-    // Ensure unique file name
-    let count = 0;
-    let fileName = `temp_video.mp4`;
-    while (fs.existsSync(fileName)) {
-      count++;
-      fileName = `temp_video_${count}.mp4`;
-    }
-
-    const videoStream = ytdl.downloadFromInfo(info, {
-      format: format,
-      filter: 'videoandaudio',
-    });
-
-    const writeStream = fs.createWriteStream(fileName);
-
-    // Pipe video stream to file
-    videoStream.pipe(writeStream);
-
-    // Promisify the writeStream finish event
-    return new Promise((resolve, reject) => {
-      writeStream.on('finish', async () => {
-        if (config.autocrop) {
-          try {
-            const croppedFileName = await autoCrop(fileName);
-            resolve(croppedFileName);
-          } catch (error) {
-            reject(error);
-          }
-        } else {
-          resolve(fileName);
-        }
-      });
-      writeStream.on('error', reject);
-    });
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-}
-
-// Helper function to get content length
-async function getContentLength(url) {
-  try {
-    const response = await axios.head(url);
-    const contentLength = response.headers['content-length'];
-    console.log(`Content-Length: ${contentLength} bytes`);
-    return contentLength;
-  } catch (error) {
-    console.error(`Error fetching content length: ${error.message}`);
-  }
-}
 
 function extractUrlFromString(text) {
   const urlRegex = /(https?:\/\/[^\s]+)/;
@@ -273,5 +259,9 @@ async function autoCrop(fileName) {
     }
   }
 }
+
+
+
+MediaDownloader.isVideoLink = isVideoLink;
 
 module.exports = MediaDownloader;
